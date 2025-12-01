a2021ca2c81b5f43ea2bbcee5cdc0951da6a97f6
Christian F <chris.1fermin@gmail.com>
2025-12-01 04:15:03 -0500

    Update Order.java

--- src/main/java/com/bobatea/momandpops/backend/models/Order.java
+++ src/main/java/com/bobatea/momandpops/backend/models/Order.java
@@ -1,39 +1,39 @@
-package com.bobatea.momandpops.backend.models;
+private String date;
+public Order(int id,
+             int customerId,
+             Integer employeeId,
+             Integer receiptId,
+             double totalCost,
+             String paymentMethod,
+             String orderType,
+             boolean completionStatus,
+             String deliveryAddress,
+             String deliveryInstructions,
+             String arrivalTime,
+             int rewardPointsEarned,
+             int rewardPointsUsed,
+             String date) {
 
-public class Order {
-    private int id;
-    private int customerId;
-    private Integer employeeId;
-    private Integer receiptId;
-    private double totalCost;
-    private String paymentMethod;
-    private String orderType;
-    private boolean completionStatus;
-    private String deliveryAddress;
-    private String deliveryInstructions;
-    private String arrivalTime;
-    private int rewardPointsEarned;
-    private int rewardPointsUsed;
+    this.id = id;
+    this.customerId = customerId;
+    this.employeeId = employeeId;
+    this.receiptId = receiptId;
+    this.totalCost = totalCost;
+    this.paymentMethod = paymentMethod;
+    this.orderType = orderType;
+    this.completionStatus = completionStatus;
+    this.deliveryAddress = deliveryAddress;
+    this.deliveryInstructions = deliveryInstructions;
+    this.arrivalTime = arrivalTime;
+    this.rewardPointsEarned = rewardPointsEarned;
+    this.rewardPointsUsed = rewardPointsUsed;
+    this.date = date;
+}
+public String getDate() {
+    return date;
+}
 
-    public Order(int id, int customerId, Integer employeeId, Integer receiptId,
-                 double totalCost, String paymentMethod, String orderType,
-                 boolean completionStatus, String deliveryAddress,
-                 String deliveryInstructions, String arrivalTime,
-                 int rewardPointsEarned, int rewardPointsUsed) {
-
-        this.id = id;
-        this.customerId = customerId;
-        this.employeeId = employeeId;
-        this.receiptId = receiptId;
-        this.totalCost = totalCost;
-        this.paymentMethod = paymentMethod;
-        this.orderType = orderType;
-        this.completionStatus = completionStatus;
-        this.deliveryAddress = deliveryAddress;
-        this.deliveryInstructions = deliveryInstructions;
-        this.arrivalTime = arrivalTime;
-        this.rewardPointsEarned = rewardPointsEarned;
-        this.rewardPointsUsed = rewardPointsUsed;
-    }
+public void setDate(String date) {
+    this.date = date;
 }
 
