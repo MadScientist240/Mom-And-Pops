b6b02cdc9320c6dde0a62311793d8347a0e4c315
Christian F <chris.1fermin@gmail.com>
2025-12-01 02:56:38 -0500

    Update Order.java

--- src/main/java/com/bobatea/momandpops/backend/models/Order.java
+++ src/main/java/com/bobatea/momandpops/backend/models/Order.java
@@ -16,24 +16,35 @@ public class Order {
     private int rewardPointsUsed;
     private String date;
 
-    public Order(int id, int customerId, Integer employeeId, Integer receiptId, double totalCost,
-                 String paymentMethod, String orderType, boolean completionStatus, String deliveryAddress,
-                 String deliveryInstructions, String arrivalTime, int rewardPointsEarned, int rewardPointsUsed, String date) {
-        this.id = id;
-        this.customerId = customerId;
-        this.employeeId = employeeId;
-        this.receiptId = receiptId;
-        this.totalCost = totalCost;
-        this.paymentMethod = paymentMethod;
-        this.orderType = orderType;
-        this.completionStatus = completionStatus;
-        this.deliveryAddress = deliveryAddress;
-        this.deliveryInstructions = deliveryInstructions;
-        this.arrivalTime = arrivalTime;
-        this.rewardPointsEarned = rewardPointsEarned;
-        this.rewardPointsUsed = rewardPointsUsed;
-        this.date = date;
-    }
+    public Order(int id,
+             int customerId,
+             Integer employeeId,
+             Integer receiptId,
+             double totalCost,
+             String paymentMethod,
+             String orderType,
+             boolean completionStatus,
+             String deliveryAddress,
+             String deliveryInstructions,
+             String arrivalTime,
+             int rewardPointsEarned,
+             int rewardPointsUsed) {
+
+    this.id = id;
+    this.customerId = customerId;
+    this.employeeId = employeeId;
+    this.receiptId = receiptId;
+    this.totalCost = totalCost;
+    this.paymentMethod = paymentMethod;
+    this.orderType = orderType;
+    this.completionStatus = completionStatus;
+    this.deliveryAddress = deliveryAddress;
+    this.deliveryInstructions = deliveryInstructions;
+    this.arrivalTime = arrivalTime;
+    this.rewardPointsEarned = rewardPointsEarned;
+    this.rewardPointsUsed = rewardPointsUsed;
+}
+
 
     public int getId() {
         return id;
