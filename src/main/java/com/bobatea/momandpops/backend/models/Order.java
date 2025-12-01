ca0ff0567de28d042076a10d451f2c5ae004970e
Christian F <chris.1fermin@gmail.com>
2025-12-01 03:05:33 -0500

    Update Order.java

--- src/main/java/com/bobatea/momandpops/backend/models/Order.java
+++ src/main/java/com/bobatea/momandpops/backend/models/Order.java
@@ -1,22 +1,4 @@
-package com.bobatea.momandpops.backend.models;
-
-public class Order {
-    private int id;
-    private int customerId;
-    private Integer employeeId; // can be null
-    private Integer receiptId;  // can be null
-    private double totalCost;
-    private String paymentMethod;
-    private String orderType;
-    private boolean completionStatus = false;
-    private String deliveryAddress;
-    private String deliveryInstructions;
-    private String arrivalTime;
-    private int rewardPointsEarned;
-    private int rewardPointsUsed;
-    private String date;
-
-    public Order(int id,
+public Order(int id,
              int customerId,
              Integer employeeId,
              Integer receiptId,
@@ -29,7 +11,6 @@ public class Order {
              String arrivalTime,
              int rewardPointsEarned,
              int rewardPointsUsed) {
-
     this.id = id;
     this.customerId = customerId;
     this.employeeId = employeeId;
@@ -43,118 +24,4 @@ public class Order {
     this.arrivalTime = arrivalTime;
     this.rewardPointsEarned = rewardPointsEarned;
     this.rewardPointsUsed = rewardPointsUsed;
-}
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(int customerId) {
-        this.customerId = customerId;
-    }
-
-    public Integer getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(Integer employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public Integer getReceiptId() {
-        return receiptId;
-    }
-
-    public void setReceiptId(Integer receiptId) {
-        this.receiptId = receiptId;
-    }
-
-    public double getTotalCost() {
-        return totalCost;
-    }
-
-    public void setTotalCost(double totalCost) {
-        this.totalCost = totalCost;
-    }
-
-    public String getPaymentMethod() {
-        return paymentMethod;
-    }
-
-    public void setPaymentMethod(String paymentMethod) {
-        this.paymentMethod = paymentMethod;
-    }
-
-    public String getOrderType() {
-        return orderType;
-    }
-
-    public void setOrderType(String orderType) {
-        this.orderType = orderType;
-    }
-
-    public boolean getCompletionStatus() {
-        return completionStatus;
-    }
-
-    public void setCompletionStatus(boolean completionStatus) {
-        this.completionStatus = completionStatus;
-    }
-
-    public String getDeliveryAddress() {
-        return deliveryAddress;
-    }
-
-    public void setDeliveryAddress(String deliveryAddress) {
-        this.deliveryAddress = deliveryAddress;
-    }
-
-    public String getDeliveryInstructions() {
-        return deliveryInstructions;
-    }
-
-    public void setDeliveryInstructions(String deliveryInstructions) {
-        this.deliveryInstructions = deliveryInstructions;
-    }
-
-    public String getArrivalTime() {
-        return arrivalTime;
-    }
-
-    public void setArrivalTime(String arrivalTime) {
-        this.arrivalTime = arrivalTime;
-    }
-
-    public int getRewardPointsEarned() {
-        return rewardPointsEarned;
-    }
-
-    public void setRewardPointsEarned(int rewardPointsEarned) {
-        this.rewardPointsEarned = rewardPointsEarned;
-    }
-
-    public int getRewardPointsUsed() {
-        return rewardPointsUsed;
-    }
-
-    public void setRewardPointsUsed(int rewardPointsUsed) {
-        this.rewardPointsUsed = rewardPointsUsed;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
 }
